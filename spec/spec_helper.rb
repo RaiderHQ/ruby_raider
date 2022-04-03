6a97c310fc596bb8247688fe8adda65afbb76176
aguspe <agustin.pe94@gmail.com>
2022-04-03 15:45:35 +0200

    All tests running green

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,3 @@ require_relative '../lib/generators/projects/rspec_project_generator'
 require_relative '../lib/generators/projects/cucumber_project_generator'
 require 'fileutils'
 require 'rspec'
-
-RSpec.configure do |config|
-
-  @project_name = 'test'
-
-  config.before(:each) do
-    RubyRaider::RspecProjectGenerator.generate_rspec_project(@project_name, automation: 'selenium')
-  end
-
-  #config.after(:each) do
-  # FileUtils.rm_rf(@project_name)
-  # end
-end
