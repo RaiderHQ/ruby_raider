2480ebda4fe181097b3c309d4705e14e4b15b6be
aguspe <agustin.pe94@gmail.com>
2022-04-10 16:38:16 +0200

    Added appium support for ios with rspec

--- lib/generators/templates/automation/abstract_page_template.rb
+++ lib/generators/templates/automation/abstract_page_template.rb
@@ -1,48 +1,79 @@
 require_relative '../template'
 
 class AbstractPageTemplate < Template
-  def body
-    browser = "def browser
-    Raider::BrowserHelper.browser
-  end"
-    driver = "def driver
-    Raider::DriverHelper.driver
-  end"
 
-    visit = @automation == 'watir' ? 'browser.goto full_url(page.first)' : 'driver.navigate.to full_url(page.first)'
-    raider = if @framework == 'rspec'
-               "require_relative '../../helpers/raider'"
-             else
-               "require_relative '../../features/support/helpers/raider'"
-             end
+  def raider
+    if @framework == 'rspec'
+      "require_relative '../../helpers/raider'"
+    else
+      "require_relative '../../features/support/helpers/raider'"
+    end
+  end
 
-    <<~EOF
-        require 'rspec'
-        #{raider}
+  def helper_selector
+    if @automation == 'watir'
+      <<~EOF
 
-        class AbstractPage
-
-          include RSpec::Matchers
-          extend Raider::PomHelper
-
-          #{@automation == 'watir' ? browser : driver}
-
-          def visit(*page)
-            #{visit}
-          end
-
-          def full_url(*page)
-            "#\{base_url}#\{url(*page)}"
-          end
-
-          def base_url
-            'https://automationteststore.com/'
-          end
-
-          def url(_page)
-            raise 'Url must be defined on child pages'
-          end
+        def browser
+          Raider::BrowserHelper.browser
         end
+      EOF
+    else
+      <<~EOF
+
+        def driver
+          Raider::DriverHelper.driver
+        end
+      EOF
+    end
+  end
+
+  def visit
+    if %w[selenium watir].include?(@automation)
+      <<~EOF
+
+        def visit(*page)
+          #{@automation == 'selenium' ? 'driver.navigate.to full_url(page.first)' : 'browser.goto full_url(page.first)'}
+        end
+      EOF
+    end
+  end
+
+  def url_methods
+    methods = <<~EOF
+
+      def full_url(*page)
+        "#\{base_url}#\{url(*page)}"
+      end
+
+      def base_url
+        'https://automationteststore.com/'
+      end
+
+      def url(_page)
+        raise 'Url must be defined on child pages'
+      end
+    EOF
+
+    if %w[selenium watir].include?(@automation)
+      methods
+    else
+      ''
+    end
+  end
+
+  def body
+    <<~EOF
+      require 'rspec'
+      #{raider}
+      class AbstractPage
+
+        include RSpec::Matchers
+        extend Raider::PomHelper
+        #{helper_selector}
+        #{visit}
+        #{url_methods}
+      end
     EOF
   end
 end
