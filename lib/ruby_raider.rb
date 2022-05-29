2461482d1dbead57a3a51ec4c5dfdd53a02102b8
aguspe <agustin.pe94@gmail.com>
2022-05-29 21:44:36 +0200

    Add ability to delete page objects and aliases to options

--- lib/ruby_raider.rb
+++ lib/ruby_raider.rb
@@ -6,43 +6,62 @@ require_relative '../lib/utilities/utilities'
 
 class RubyRaider < Thor
   desc "new [PROJECT_NAME]", "Creates a new framework based on settings picked"
+
   def new(project_name)
     MenuGenerator.generate_choice_menu(project_name)
   end
 
   desc "page [PAGE_NAME]", "Creates a new page object"
-  option :path, :type => :string, :required => false, :desc => 'The path were your page will be created'
+  option :path, :type => :string, :required => false, :desc => 'The path where your page will be created', :aliases => '-p'
+  option :actions, :type => :array, :required => false, :desc => 'The actions that you can perform on your page', :aliases => '-a'
+  option :elements, :type => :array, :required => false, :desc => 'The elements that you can use on your page', :aliases => '-e'
+  option :delete, :type => :boolean, :required => false, :desc => 'This will delete the selected page', :aliases => '-d'
+
   def page(name)
     path = options[:path].nil? ? load_config_path : options[:path]
-    Scaffolding.new([name, path]).generate_class
+    if options[:delete]
+      Scaffolding.new([name, path]).delete_class
+    else
+      Scaffolding.new([name, path]).generate_class
+    end
   end
 
   desc "feature [FEATURE_NAME]", "Creates a new feature"
+  option :path, :type => :string, :required => false, :desc => 'The path where your feature will be created'
+
   def feature(name)
-    Scaffolding.new([name, load_config_path]).generate_feature
+    path = options[:path].nil? ? load_config_path : options[:path]
+    Scaffolding.new([name, path]).generate_feature
   end
 
   desc "spec [SPEC_NAME]", "Creates a new spec"
+  option :path, :type => :string, :required => false, :desc => 'The path where your spec will be created'
+
   def spec(name)
+    path = options[:path].nil? ? load_config_path : options[:path]
     Scaffolding.new([name, load_config_path]).generate_spec
   end
 
   desc "path [PATH]", "Sets the default path for scaffolding"
+
   def path(default_path)
     Utilities.new.path = default_path
   end
 
   desc "url [URL]", "Sets the default url for a project"
+
   def url(default_url)
     Utilities.new.url = default_url
   end
 
   desc "browser [BROWSER]", "Sets the default browser for a project"
+
   def browser(default_browser)
     Utilities.new.browser = default_browser
   end
 
   desc "raid", "It runs all the tests in a project"
+
   def raid
     Utilities.new.run
   end
