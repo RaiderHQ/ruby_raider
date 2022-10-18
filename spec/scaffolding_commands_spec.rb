a9878ed1ce5c00ef39f5fecc6f0bd6dd2c5c977f
aguspe <agustin.pe94@gmail.com>
2022-10-18 20:51:16 +0200

    Fix scaffolding tests

--- spec/scaffolding_commands_spec.rb
+++ spec/scaffolding_commands_spec.rb
@@ -2,13 +2,28 @@ require 'pathname'
 require 'yaml'
 require_relative '../lib/generators/common_generator'
 require_relative '../lib/commands/scaffolding_commands'
+require_relative '../lib/scaffolding/scaffolding'
 require_relative 'spec_helper'
 
 describe ScaffoldingCommands do
   let(:scaffold) { described_class }
   let(:name) { 'test' }
 
+  orig_dir = Dir.pwd
+
+  after do
+    Dir.chdir orig_dir
+  end
+
   context 'with a spec folder' do
+    let(:new_path) { 'test_folder' }
+
+    path = "#{FRAMEWORKS.last}_#{AUTOMATION_TYPES[2]}"
+
+    before do
+      Dir.chdir path
+    end
+
     it 'scaffolds for rspec creating a spec' do
       scaffold.new.invoke(:scaffold, nil, %W[#{name}])
       expect(Pathname.new("spec/#{name}_spec.rb")).to be_file
@@ -18,36 +33,74 @@ describe ScaffoldingCommands do
       scaffold.new.invoke(:scaffold, nil, %W[#{name}])
       expect(Pathname.new("page_objects/pages/#{name}_page.rb")).to be_file
     end
-  end
 
-  context 'with a features folder' do
-    it 'scaffolds for cucumber creating a feature' do
-      FileUtils.mkdir 'features'
-      scaffold.new.invoke(:scaffold, nil, %W[#{name}])
-      expect(Pathname.new("features/#{name}.feature")).to be_file
+    it 'deletes a spec' do
+      scaffold.new.invoke(:spec, nil, %w[login --delete])
+      expect(Pathname.new('spec/login_spec.rb')).not_to be_file
     end
 
-    it 'scaffolds for cucumber creating a page' do
-      FileUtils.mkdir 'features'
-      scaffold.new.invoke(:scaffold, nil, %W[#{name}])
-      expect(Pathname.new("page_objects/pages/#{name}_page.rb")).to be_file
+    it 'deletes a helper' do
+      scaffold.new.invoke(:helper, nil, %w[driver --delete])
+      expect(Pathname.new('helpers/driver_helper.rb')).not_to be_file
     end
-  end
 
-  context 'with path from config file' do
     it 'creates a page' do
       scaffold.new.invoke(:page, nil, %W[#{name}])
       expect(Pathname.new("page_objects/pages/#{name}_page.rb")).to be_file
     end
 
-    it 'creates a feature' do
-      scaffold.new.invoke(:feature, nil, %W[#{name}])
-      expect(Pathname.new("features/#{name}.feature")).to be_file
+    it 'changes the path for specs' do
+      scaffold.new.invoke(:path, nil, %W[#{path} -s])
+      config = YAML.load_file('config/config.yml')
+      expect(config['spec_path']).to eql path
+    end
+
+    it 'updates the url' do
+      scaffold.new.invoke(:url, nil, %w[test.com])
+      config = YAML.load_file('config/config.yml')
+      expect(config['url']).to eql 'test.com'
+    end
+
+    it 'updates the browser' do
+      scaffold.new.invoke(:browser, nil, %w[:firefox])
+      config = YAML.load_file('config/config.yml')
+      expect(config['browser']).to eql ':firefox'
+    end
+
+    it 'updates the browser options' do
+      scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless start-maximized start-fullscreen])
+      config = YAML.load_file('config/config.yml')
+      expect(config['browser_options']).to eql %w[headless start-maximized start-fullscreen]
+    end
+
+    it 'creates a page with a path' do
+      scaffold.new.invoke(:page, nil, %W[#{name} --path #{new_path}])
+      expect(Pathname.new("#{new_path}/#{name}_page.rb")).to be_file
     end
 
     it 'creates a spec' do
-      scaffold.new.invoke(:spec, nil, %W[#{name}])
-      expect(Pathname.new("spec/#{name}_spec.rb")).to be_file
+      scaffold.new.invoke(:spec, nil, %W[#{name} --path #{new_path}])
+      expect(Pathname.new("#{new_path}/#{name}_spec.rb")).to be_file
+    end
+  end
+
+  context 'with a features folder' do
+    let(:new_path) { 'test_folder' }
+
+    path = "#{FRAMEWORKS.first}_#{AUTOMATION_TYPES.last}"
+
+    before do
+      Dir.chdir path
+    end
+
+    it 'scaffolds for cucumber creating a feature' do
+      scaffold.new.invoke(:scaffold, nil, %W[#{name}])
+      expect(Pathname.new("features/#{name}.feature")).to be_file
+    end
+
+    it 'scaffolds for cucumber creating a page' do
+      scaffold.new.invoke(:scaffold, nil, %W[#{name}])
+      expect(Pathname.new("page_objects/pages/#{name}_page.rb")).to be_file
     end
 
     it 'creates a helper' do
@@ -56,56 +109,14 @@ describe ScaffoldingCommands do
     end
 
     it 'deletes a page' do
-      scaffold.new.invoke(:page, nil, %W[#{name}])
-      scaffold.new.invoke(:page, nil, %W[#{name} --delete])
-      expect(Pathname.new("page_objects/pages/#{name}_page.rb")).not_to be_file
+      scaffold.new.invoke(:page, nil, %w[login --delete])
+      expect(Pathname.new('page_objects/pages/login_page.rb')).not_to be_file
     end
 
     it 'deletes a feature' do
-      scaffold.new.invoke(:feature, nil, %W[#{name}])
-      scaffold.new.invoke(:feature, nil, %W[#{name} --delete])
-      expect(Pathname.new("features/#{name}.feature")).not_to be_file
+      scaffold.new.invoke(:feature, nil, %w[login --delete])
+      expect(Pathname.new('features/login.feature')).not_to be_file
     end
-
-    it 'deletes a spec' do
-      scaffold.new.invoke(:spec, nil, %W[#{name}])
-      scaffold.new.invoke(:spec, nil, %W[#{name} --delete])
-      expect(Pathname.new("spec/#{name}_spec.rb")).not_to be_file
-    end
-
-    it 'deletes a helper' do
-      scaffold.new.invoke(:helper, nil, %W[#{name}])
-      scaffold.new.invoke(:helper, nil, %W[#{name} --delete])
-      expect(Pathname.new("helpers/#{name}_helper.rb")).not_to be_file
-    end
-  end
-
-  context 'with path option' do
-    let(:path) { 'test_folder' }
-
-    it 'creates a page' do
-      scaffold.new.invoke(:page, nil, %W[#{name} --path #{path}])
-      expect(Pathname.new("#{path}/#{name}_page.rb")).to be_file
-    end
-
-    it 'creates a feature' do
-      scaffold.new.invoke(:feature, nil, %W[#{name} --path #{path}])
-      expect(Pathname.new("#{path}/#{name}.feature")).to be_file
-    end
-
-    it 'creates a spec' do
-      scaffold.new.invoke(:spec, nil, %W[#{name} --path #{path}])
-      expect(Pathname.new("#{path}/#{name}_spec.rb")).to be_file
-    end
-
-    it 'creates a helper' do
-      scaffold.new.invoke(:helper, nil, %W[#{name} --path #{path}])
-      expect(Pathname.new("#{path}/#{name}_helper.rb")).to be_file
-    end
-  end
-
-  context 'without default path' do
-    let(:path) { 'test_folder' }
 
     it 'changes the path for pages' do
       scaffold.new.invoke(:path, nil, %W[#{path}])
@@ -119,71 +130,33 @@ describe ScaffoldingCommands do
       expect(config['feature_path']).to eql path
     end
 
-    it 'changes the path for specs' do
-      scaffold.new.invoke(:path, nil, %W[#{path} -s])
+    it 'updates only the browser options' do
+      scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless])
       config = YAML.load_file('config/config.yml')
-      expect(config['spec_path']).to eql path
+      expect(config['browser_options']).to eql %w[headless]
     end
 
-    it 'creates page' do
-      scaffold.new.invoke(:path, nil, %W[#{path}])
-      scaffold.new.invoke(:page, nil, %W[#{name}])
-      expect(Pathname.new("#{path}/#{name}_page.rb")).to be_file
+    it 'deletes the browser options when passed with the delete parameter' do
+      scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless --delete])
+      config = YAML.load_file('config/config.yml')
+      expect(config['browser_options']).to be_nil
     end
 
-    it 'creates feature' do
-      scaffold.new.invoke(:path, nil, %W[#{path} -f])
-      scaffold.new.invoke(:feature, nil, %W[#{name}])
-      expect(Pathname.new("#{path}/#{name}.feature")).to be_file
+    it 'deletes the browser options' do
+      scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless])
+      scaffold.new.invoke(:browser, nil, %w[--delete])
+      config = YAML.load_file('config/config.yml')
+      expect(config['browser_options']).to be_nil
     end
 
-    it 'creates spec' do
-      scaffold.new.invoke(:path, nil, %W[#{path} -s])
-      scaffold.new.invoke(:spec, nil, %W[#{name}])
-      expect(Pathname.new("#{path}/#{name}_spec.rb")).to be_file
+    it 'creates a feature' do
+      scaffold.new.invoke(:feature, nil, %W[#{name} --path #{new_path}])
+      expect(Pathname.new("#{new_path}/#{name}.feature")).to be_file
     end
 
-    it 'creates a helper' do
-      scaffold.new.invoke(:path, nil, %W[#{path} -h])
-      scaffold.new.invoke(:helper, nil, %W[#{name}])
-      expect(Pathname.new("#{path}/#{name}_helper.rb")).to be_file
+    it 'creates a helper with a path' do
+      scaffold.new.invoke(:helper, nil, %W[#{name} --path #{new_path}])
+      expect(Pathname.new("#{new_path}/#{name}_helper.rb")).to be_file
     end
-  end
-
-  it 'updates the url' do
-    scaffold.new.invoke(:url, nil, %w[test.com])
-    config = YAML.load_file('config/config.yml')
-    expect(config['url']).to eql 'test.com'
-  end
-
-  it 'updates the browser' do
-    scaffold.new.invoke(:browser, nil, %w[:firefox])
-    config = YAML.load_file('config/config.yml')
-    expect(config['browser']).to eql ':firefox'
-  end
-
-  it 'updates the browser options' do
-    scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless start-maximized start-fullscreen])
-    config = YAML.load_file('config/config.yml')
-    expect(config['browser_options']).to eql %w[headless start-maximized start-fullscreen]
-  end
-
-  it 'updates only the browser options' do
-    scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless])
-    config = YAML.load_file('config/config.yml')
-    expect(config['browser_options']).to eql %w[headless]
-  end
-
-  it 'deletes the browser options when passed with the delete parameter' do
-    scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless --delete])
-    config = YAML.load_file('config/config.yml')
-    expect(config['browser_options']).to be_nil
-  end
-
-  it 'deletes the browser options' do
-    scaffold.new.invoke(:browser, nil, %w[:firefox --opts headless])
-    scaffold.new.invoke(:browser, nil, %w[--delete])
-    config = YAML.load_file('config/config.yml')
-    expect(config['browser_options']).to be_nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 require 'fileutils'
 require 'rspec'
+require_relative '../lib/generators/menu_generator'
 
 AUTOMATION_TYPES = %w[android ios selenium watir].freeze
 FRAMEWORKS = %w[cucumber rspec].freeze
@@ -10,7 +11,7 @@ RSpec.configure do |config|
   config.before(:all) do
     FRAMEWORKS.each do |framework|
       AUTOMATION_TYPES.each do |automation|
-        described_class.new([automation, framework, "#{framework}_#{automation}"]).invoke_all
+        MenuGenerator.new("#{framework}_#{automation}").generate_framework(automation, framework)
       end
     end
   end
