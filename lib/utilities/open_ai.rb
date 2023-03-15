65b9fc96418943826aeef2fb6c976ce16e13fc06
Agustin Pequeno <33221555+aguspe@users.noreply.github.com>
2023-03-15 20:21:34 +0100

    Add open ai support (#40)
    * Add support for open ai
    
    * Finish adding initial support for open ai
    
    ---------
    
    Co-authored-by: aguspe <agustin.pe94@gmail.com>

---
+++ lib/utilities/open_ai.rb
@@ -0,0 +1,37 @@
+require 'openai'
+require 'fileutils'
+
+module OpenAi
+  class << self
+    def create_client
+      configure_client
+      OpenAI::Client.new
+    end
+
+    def configure_client
+      OpenAI.configure do |config|
+        config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+        config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID', nil)
+      end
+    end
+
+    def input(model = "gpt-3.5-turbo", temperature = 0.7, request)
+      create_client.chat(
+        parameters: {
+          model: model,
+          messages: [{ role: "user", content: request }],
+          temperature: temperature
+        })
+    end
+
+    def create_file(choice = 0, path, request)
+      response = input(request)
+      File.write(path, response.dig("choices", choice, "message", "content"))
+    end
+
+    def output(choice = 0, request)
+      response = input(request)
+      puts response.dig("choices", choice, "message", "content")
+    end
+  end
+end
