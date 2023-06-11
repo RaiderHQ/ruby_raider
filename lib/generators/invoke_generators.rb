62308ab0e98dd1527f2f7b4f639c50495713a8df
Agustin Pequeno <33221555+aguspe@users.noreply.github.com>
2023-06-11 11:51:15 +0200

    Improve module inclusion on frameworks (#58)
    * Improve module inclusion on frameworks
    
    * Upgrade versions and support visual testing better
    
    ---------
    
    Co-authored-by: aguspe <agustin.pe94@gmail.com>

--- lib/generators/invoke_generators.rb
+++ lib/generators/invoke_generators.rb
@@ -1,11 +1,8 @@
 require_relative 'automation/automation_generator'
-require_relative 'automation/automation_examples_generator'
 require_relative 'common_generator'
 require_relative 'cucumber/cucumber_generator'
-require_relative 'cucumber/cucumber_examples_generator'
 require_relative 'helper_generator'
 require_relative 'rspec/rspec_generator'
-require_relative 'rspec/rspec_examples_generator'
 
 # :reek:FeatureEnvy { enabled: false }
 # :reek:UtilityFunction { enabled: false }
@@ -14,7 +11,6 @@ module InvokeGenerators
     generators = %w[Automation Common Helpers]
     framework = structure[:framework]
     add_generator(generators, framework.capitalize)
-    generators = add_examples(generators) if structure[:examples]
     generators.each do |generator|
       invoke_generator({
                          automation: structure[:automation],
@@ -28,16 +24,6 @@ module InvokeGenerators
 
   def add_generator(generators, *gens)
     gens.each { |generator| generators.push generator }
-  end
-
-  def add_examples(generators)
-    if generators.include?('Cucumber')
-      generators.push('CucumberExamples')
-    else
-      generators.push('RspecExamples')
-    end
-
-    generators.push('AutomationExamples')
   end
 
   def invoke_generator(structure = {})
