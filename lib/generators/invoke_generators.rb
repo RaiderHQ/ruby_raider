c15ed91ac6aae0d50902eeeda97e16d5a99c8d96
Augustin Gottlieb Pequeno <33221555+aguspe@users.noreply.github.com>
2024-06-30 00:16:36 +0200

    Test selenium workflow (#100)
    * Test selenium workflow
    
    * Update flow
    
    * Updare ruby raider
    
    * Retry flow
    
    * Retry flow
    
    * Retry flow
    
    * Add command to pass parameters
    
    * Update flow
    
    * Update tests
    
    * Update tests
    
    * Update tests
    
    * Update tests
    
    * Update tests
    
    * Update tests
    
    * Update version
    
    * Update lib/ruby_raider.rb
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Correct rubocop and reek issues
    
    * Update rubocop
    
    * Fix rubocop
    
    ---------
    
    Co-authored-by: aguspe <agustin.pe94@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/generators/invoke_generators.rb
+++ lib/generators/invoke_generators.rb
@@ -8,6 +8,8 @@ require_relative 'rspec/rspec_generator'
 # :reek:FeatureEnvy { enabled: false }
 # :reek:UtilityFunction { enabled: false }
 module InvokeGenerators
+  module_function
+
   def generate_framework(structure = {})
     generators = %w[Automation Actions Common Helpers]
     framework = structure[:framework]
@@ -15,11 +17,11 @@ module InvokeGenerators
     generators.each do |generator|
       invoke_generator({
                          automation: structure[:automation],
-                         framework: framework,
-                         generator: generator,
+                         framework:,
+                         generator:,
                          name: structure[:name],
-                         visual: structure[:visual],
-                         axe_support: structure[:axe_support]
+                         visual: to_bool(structure[:visual]),
+                         axe_support: to_bool(structure[:axe_support])
                        })
     end
   end
@@ -35,5 +37,11 @@ module InvokeGenerators
                 structure[:name],
                 structure[:visual],
                 structure[:axe_support]]).invoke_all
+  end
+
+  def to_bool(string)
+    return unless string.is_a? String
+
+    string.downcase == 'true'
   end
 end
